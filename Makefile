8d09e379b24a702383dc890c33cf45ea245679ac
Helen Yu <hyu@student.42.us.org>
2020-01-30 17:56:36 -0800

    Makefile

--- Makefile
+++ Makefile
@@ -6,52 +6,57 @@
 #    By: hyu <marvin@42.fr>                         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/27 19:29:57 by hyu               #+#    #+#              #
-#    Updated: 2020/01/28 12:14:22 by hyu              ###   ########.fr        #
+#    Updated: 2020/01/30 16:41:37 by hyu              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME=fdf
 
-SRCS=deal_key.c\
-	 get_next_line.c\
-	 input_check.c\
-	 linked_list.c\
-	 read_file.c\
-	 rescale.c\
-	 scaling.c\
-	 t_scaler.c\
-	 word_spacecheck.c
+SRCS=./srcs/deal_key.c \
+	 ./srcs/get_next_line.c \
+	 ./srcs/input_check.c \
+	 ./srcs/linked_list.c \
+	 ./srcs/read_file.c \
+	 ./srcs/rescale.c \
+	 ./srcs/scaling.c \
+	 ./srcs/t_scaler.c \
+	 ./srcs/word_spacecheck.c \
+	 ./srcs/twodim_linked_list.c\
+	 ./srcs/hex.c
 	  
-OBJECTS=deal_key.o\
-     get_next_line.o\
-     input_check.o\
-	 linked_list.o\
-	 read_file.o\
-     rescale.o\
-     scaling.o\
-     t_scaler.o\
-     word_spacecheck.o
+OBJECTS=deal_key.o \
+     get_next_line.o \
+     input_check.o \
+	 linked_list.o \
+	 read_file.o \
+     rescale.o \
+     scaling.o \
+     t_scaler.o \
+     word_spacecheck.o \
+	 twodim_linked_list.o\
+	 hex.o
 
-INCLUDES=-I minilibx/ -I libft/
-FLAG=-Wall -Werror -Wextra -g
+INCLUDES=-I minilibx/ -I libft/ -I INCLUDES/
+FLAGS=-Wall -Werror -Wextra -g
 
 LIBFT=-L libft -l ft
 FRAMEWORK=-framework OpenGL -framework Appkit
 MLX=-L ~/Desktop/minilibx_macos -l mlx
 
 all: $(NAME)
+
 $(NAME):
 	make -C libft
 	make -C minilibx
-	cc $(LIBFT) $(MLX) $(FRAMEWORK) $(OBJS) -o $(NAME)
+	gcc -c $(SRCS)
+	ar -rv fdf.a $(OBJECTS)
+	ranlib fdf.a
+	gcc minilibx/libmlx.a libft/libft.a fdf.a -framework OpenGL -framework Appkit main.c -o $(NAME)
 
 clean:
 	rm -rf $(OBJECTS)
-	make -C libft clean
-	make -C minilibx clean
 
 fclean: clean
 	rm -f $(NAME)
-	make -C libft fclean
-
+	
 re:fclean all
--- main.c
+++ main.c
@@ -6,69 +6,29 @@
 /*   By: hyu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 14:32:29 by hyu               #+#    #+#             */
-/*   Updated: 2020/01/27 19:23:36 by hyu              ###   ########.fr       */
+/*   Updated: 2020/01/30 16:59:43 by hyu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fdf.h"
+#include "includes/fdf.h"
 
-int		main(int argc, char **argv)
+int				main(int argc, char **argv)
 {
-	char *tile;
-	t_fpoint *iterate;
-	t_fpoint *begin;
-	t_point	*dimensions;
+	char		*tile;
+	t_fpoint	*iterate;
+	t_fpoint	*begin;
+	t_point		*dimensions;
 
 	if (argc != 2)
-    {
-        ft_putstr("Error: Invalid number of arguments");
-        exit(0);
-    }
-	dimensions = malloc(sizeof(t_point));	
+	{
+		ft_putstr("Error: Invalid number of arguments");
+		exit(0);
+	}
+	dimensions = malloc(sizeof(t_point));
 	tile = ft_error(argv[1]);
 	begin = ft_set_dimensions(tile, dimensions);
 	y_list_iterator(begin, dimensions);
 	dimensions = ft_set_scaling(dimensions, begin);
-	/*dimensions->scalex = 30;
-	dimensions->scaley = 30;
-	dimensions->scalez = 30;
-	ft_list_frch_scale(begin, re_scale, dimensions);
-	dimensions->x_trans = 100;
-	ft_list_frch_scale(begin, translation_xyz, dimensions);*/
 	graphics_display(dimensions, begin);
-//	int i;
-
-  //  i = 0;
-    //begin = begin->nexty;
-
-    //while (begin)
-
-	int i;
-
-	i = 0;
-	//begin = begin->nexty;
-
-	while (dimensions->begin)
-    {
-		//ft_putchar('a');
-	//	ft_putnbr(i);
-		//ft_putchar('x');
-		ft_putnbr(dimensions->begin->x);
-		//ft_putchar('y');
-	//	ft_putstr("hex");
-      //  ft_putnbr(begin->hex);
-		//ft_putchar('\n');
-		dimensions->begin = dimensions->begin->nexty;
-		i++;
-	}
-	
-	//y_list_iterator(begin, dimensions);
-	//ft_putchar('e');
-	/*while (begin)
-	{
-		ft_putchar('z');
-		ft_putnbr(begin->y);
-		begin = begin->next;
-	}*/
 	return (0);
 }
